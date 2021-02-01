cd27e63426c043dfb9b2b6845f35935b31feb968
Alex-S-L <61691862+Alex-S-L@users.noreply.github.com>
2021-02-01 15:13:21 +0300

    Доработки

--- js/main.js
+++ js/main.js
@@ -1,29 +1,33 @@
-const getRandomNumber = (min, max) => {
-  if (Math.sign(min) > -1 && max > min) {
-    const randomNumber = Math.random() * (max - min) + min;
-    return randomNumber
+// Решил вынести проверку чтоб не писать в 2-х функциях
+const getCorrectRange = (min, max) => {
+  if (min >= 0 && max > min) {
+    return true;
   }
-
   alert('Функций getRandomNumber получила недопустимое значение')
-  return null
+  return false;
+};
+// Пусть хоть эта формула будет корректной
+const getRandomNumber = (min, max) => {
+  if (getCorrectRange(min, max)) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 };
 
-const getWholeNumber = (min, max) => Math.round(getRandomNumber(min, max));
-
-const getFractionNumber = (min, max, afterComma) => {
-  return (getRandomNumber (min, max)).toFixed(afterComma);
-}
-// Выше неправильное решение.
-
-const getRandomWholeNumber1 = (min, max) => {
-  if (Math.sign(min) > -1 && max > min) {
-    retun Math.floor(Math.random() * (max - min + 1) + min)
+const getRandomFloat = (min, max, afterComma) => {
+  if (getCorrectRange(min,max)) {
+    // переменная нужна для улучшения читаемости
+    const randomFloat = Math.random() * (max - min) + min;
+    return +(randomFloat.toFixed(afterComma));
   }
+};
 
-  alert('Функций getRandomNumber получила недопустимое значение')
-  return null
+const randomNumbers = [];
+const randomFloats = [];
+
+for (let i = 0; i < 30; i++) {
+  randomNumbers[i] = getRandomNumber(0, 20);
+  randomFloats[i] = getRandomFloat(0, 20, 2);
 }
-// Функция для получение целого значения, как мне кажется, должна выглядеть как то так.
-// А как реализовать получение значения с плавающей точкой, да чтоб она ещё и закрывала
-// диапазон включительно как то не могу себе представить.
-// Решил набросать примерное решение чтоб было что обсудить. =)
+
+// console.log(randomNumbers);
+// console.log(randomFloats);
