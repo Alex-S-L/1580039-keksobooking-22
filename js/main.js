de9c5f5be0546f67aeda99c1bc998491a7a91b73
Alex-S-L <learning.for@mail.ru>
2021-02-07 11:16:29 +0300

    Больше данных

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-// Решил вынести проверку чтоб не писать в 2-х функциях
+'use strict'
+// Проверка корректности диапазона
 const getCorrectRange = (min, max) => {
   if (min >= 0 && max > min) {
     return true;
@@ -6,7 +7,7 @@ const getCorrectRange = (min, max) => {
   alert('Функций getRandomNumber получила недопустимое значение')
   return false;
 };
-// Пусть хоть эта формула будет корректной
+
 const getRandomNumber = (min, max) => {
   if (getCorrectRange(min, max)) {
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -14,20 +15,95 @@ const getRandomNumber = (min, max) => {
 };
 
 const getRandomFloat = (min, max, afterComma) => {
-  if (getCorrectRange(min,max)) {
-    // переменная нужна для улучшения читаемости
-    const randomFloat = Math.random() * (max - min) + min;
-    return +(randomFloat.toFixed(afterComma));
+  if (getCorrectRange(min, max)) {
+    let randomFloat = Math.random() * (max - min + (0.1 ** afterComma)) + min;/*пока самая удачная с точки зрения статистики*/
+    return Math.floor(randomFloat * 10 ** afterComma) / 10 ** afterComma;
   }
 };
+  // Тестирование формул получения рандомных чисел
+/*const getRandomArrey = (arreyLength, min, max, afterComma) => {
+  let randomArrey = [];
+  for (let i = 0; i < arreyLength; i++) {
+    randomArrey[i] = getRandomFloat(min, max, afterComma);
+  }
+  return randomArrey;
+};
 
-const randomNumbers = [];
-const randomFloats = [];
+const randomArrey = getRandomArrey(3000, 0, 5, 0);
 
-for (let i = 0; i < 30; i++) {
-  randomNumbers[i] = getRandomNumber(0, 20);
-  randomFloats[i] = getRandomFloat(0, 20, 2);
+const getResultEvaluation = (randomArrey) => {
+  const result = {};
+  for(let i = 0; i < randomArrey.length; i++) {
+    const objectProperty = randomArrey[i]
+    if (result[objectProperty] != undefined) {
+      result[objectProperty]++
+    } else {
+      result[objectProperty] = 1;
+    }
+  }
+  return result;
 }
 
-// console.log(randomNumbers);
-// console.log(randomFloats);
+const result = getResultEvaluation(randomArrey)
+
+console.log(randomArrey);
+console.log(result);
+*/
+const ADVERTISEMENT_COUNT = 10;
+const advertisements = [];
+const TYPES_OF_HOUSING = ['bungalow', 'flat', 'house', 'palace'];
+const TIME_Of_CHECKIN = ['12.00', '13.00', '14.00'];
+const TIME_Of_CHECKOUT = ['12.00', '13.00', '14.00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
+  'http://o0.github.io/assets/images/tokyo/hotel3.jpg',
+];
+const getRandomFeatures = (features) => {
+  let featuresCopy = features.slice();
+  return featuresCopy.slice(0, getRandomNumber(1, featuresCopy.length));
+};
+const getRandomPhotos = (photos) => {
+  let photosCopy = photos.slice();
+  return photosCopy.slice(0, getRandomNumber(1,photosCopy.length));
+};
+
+const getOffer = () => {
+  return {
+    title: 'Заголовок',
+    address: 'x = ' + getRandomFloat(0, 180, 5) + ', ' + 'y = ' + getRandomFloat(0, 90, 5),
+    price: getRandomNumber(1000, 1000000),
+    type: TYPES_OF_HOUSING[getRandomNumber(0, 3)],
+    rooms: getRandomNumber(0, 10),
+    guests: getRandomNumber(0, 10),
+    checkin: TIME_Of_CHECKIN[getRandomNumber(0, 2)],
+    checkout: TIME_Of_CHECKOUT[getRandomNumber(0, 2)],
+    features: getRandomFeatures(FEATURES),
+    description: 'Описание помещения',
+    photos: getRandomPhotos(PHOTOS),
+  }
+}
+
+const getLocation = () => {
+  return {
+    x: getRandomFloat(35.65, 35.7, 5),
+    y: getRandomFloat(139.7, 139.8, 5),
+  }
+}
+// Собираем всё в один объект
+const getAdvertisement = () => {
+  return {
+    author: 'img/avatars/user0' + getRandomNumber(1, 8) + '.png',
+    offer: getOffer(),
+    location: getLocation(),
+  }
+}
+
+const getAdvertisements = (count) => {
+  for (let i = 0; i < count; i++) {
+    advertisements.push(getAdvertisement());
+  }
+  return advertisements;
+}
+// просто чтоб линтер не ругался.
+alert(getAdvertisements(ADVERTISEMENT_COUNT));
